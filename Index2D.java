2179261ff3851eb4d3f7e0eda06c4b2c8e31fab6
dansu <dansuh5@gmail.com>
2025-12-27 12:51:21 +0200

    Map basic implementation: init, get/set, equals, tests passing.

--- Index2D.java
+++ Index2D.java
@@ -1,41 +1,56 @@
 package assignments.Ex2;
 
 public class Index2D implements Pixel2D {
+
+    private int x;
+    private int y;
+
     public Index2D(int w, int h) {
-        ;
+
+        this.x = w;
+        this.y = h;
     }
     public Index2D(Pixel2D other) {
-        ;
+        if (other == null) {
+            throw new NullPointerException();
+        }
+        this.x = other.getX();
+        this.y = other.getY();
     }
     @Override
     public int getX() {
 
-        return 0;
+        return x;
     }
 
     @Override
     public int getY() {
 
-        return 0;
+        return y;
     }
 
     @Override
     public double distance2D(Pixel2D p2) {
-
-        return 0;
+        if (p2 == null) {
+            throw new RuntimeException("p2 is null");
+        }
+        int dx = this.x - p2.getX();
+        int dy = this.y - p2.getY();
+        return Math.sqrt(dx * dx + dy * dy);
     }
 
     @Override
     public String toString() {
-        String ans = null;
-
-        return ans;
+        return "(" + x + "," + y + ")";
     }
 
     @Override
     public boolean equals(Object p) {
-        boolean ans = true;
+        if (this == p) return true;
+        if (p == null) return false;
+        if (!(p instanceof Index2D)) return false;
 
-        return ans;
+        Pixel2D other = (Pixel2D) p;
+        return this.x == other.getX() && this.y == other.getY();
     }
 }
--- Map.java
+++ Map.java
@@ -7,9 +7,8 @@ import java.io.Serializable;
  * @author boaz.benmoshe
  *
  */
-public class Map implements Map2D, Serializable{
-
-    // edit this class below
+public class Map implements Map2D, Serializable {
+	private int[][] map;
 	/**
 	 * Constructs a w*h 2D raster map with an init value v.
 	 * @param w
@@ -22,7 +21,7 @@ public class Map implements Map2D, Serializable{
 	 * @param size
 	 */
 	public Map(int size) {this(size,size, 0);}
-	
+
 	/**
 	 * Constructs a map from a given 2D array.
 	 * @param data
@@ -32,103 +31,134 @@ public class Map implements Map2D, Serializable{
 	}
 	@Override
 	public void init(int w, int h, int v) {
-
+		if (w <= 0 || h <= 0) {
+			throw new IllegalArgumentException();
+		}
+		map = new int[w][h];
+		for (int x = 0; x < w; x++) {
+			for (int y = 0; y < h; y++) {
+				map[x][y] = v;
+			}
+		}
 	}
 	@Override
 	public void init(int[][] arr) {
+		if (arr == null || arr.length == 0 || arr[0] == null || arr[0].length == 0) {
+			throw new RuntimeException("Bad array");
+		}
+		int w = arr.length;
+		int h = arr[0].length;
 
+		for (int x = 0; x < w; x++) {
+			if (arr[x] == null || arr[x].length != h) {
+				throw new RuntimeException("Ragged array");
+			}
+		}
+		map = new int[w][h];
+		for (int x = 0; x < w; x++) {
+			System.arraycopy(arr[x], 0, map[x], 0, h);
+		}
 	}
 	@Override
 	public int[][] getMap() {
-		int[][] ans = null;
-
-		return ans;
+		int w = getWidth();
+		int h = getHeight();
+		int[][] copy = new int[w][h];
+		for (int x = 0; x < w; x++) {
+			System.arraycopy(map[x], 0, copy[x], 0, h);
+		}
+		return copy;
 	}
 	@Override
 	public int getWidth() {
-        int ans = -1;
-
-        return ans;
-    }
+	return map.length;
+	}
 	@Override
 	public int getHeight() {
-        int ans = -1;
-
-        return ans;
-    }
+		return map[0].length;
+	}
 	@Override
 	public int getPixel(int x, int y) {
-        int ans = -1;
-
-        return ans;
-    }
+		return map[x][y];
+	}
 	@Override
 	public int getPixel(Pixel2D p) {
-        int ans = -1;
-
-        return ans;
+		if (p == null) throw new RuntimeException("p is null");
+		return getPixel(p.getX(), p.getY());
 	}
 	@Override
 	public void setPixel(int x, int y, int v) {
-
-    }
+		map[x][y] = v;
+	}
 	@Override
 	public void setPixel(Pixel2D p, int v) {
+		if (p == null) throw new RuntimeException("p is null");
+		setPixel(p.getX(), p.getY(), v);
+	}
+
+	@Override
+	public boolean isInside(Pixel2D p) {
+		if (p == null) return false;
+		int x = p.getX();
+		int y = p.getY();
+		return x >= 0 && x < getWidth() && y >= 0 && y < getHeight();
+	}
+
+	@Override
+	public boolean sameDimensions(Map2D p) {
+		if (p == null) return false;
+		return this.getWidth() == p.getWidth() && this.getHeight() == p.getHeight();
+	}
+
+	@Override
+	public void addMap2D(Map2D p) {
 
 	}
 
-    @Override
-    public boolean isInside(Pixel2D p) {
-        boolean ans = true;
-
-        return ans;
-    }
-
-    @Override
-    public boolean sameDimensions(Map2D p) {
-        boolean ans = false;
-
-        return ans;
-    }
-
-    @Override
-    public void addMap2D(Map2D p) {
-
-    }
-
-    @Override
-    public void mul(double scalar) {
-
-    }
-
-    @Override
-    public void rescale(double sx, double sy) {
-
-    }
-
-    @Override
-    public void drawCircle(Pixel2D center, double rad, int color) {
-
-    }
-
-    @Override
-    public void drawLine(Pixel2D p1, Pixel2D p2, int color) {
-
-    }
-
-    @Override
-    public void drawRect(Pixel2D p1, Pixel2D p2, int color) {
-
-    }
-
-    @Override
-    public boolean equals(Object ob) {
-        boolean ans = false;
-
-        return ans;
-    }
 	@Override
-	/** 
+	public void mul(double scalar) {
+
+	}
+
+	@Override
+	public void rescale(double sx, double sy) {
+
+	}
+
+	@Override
+	public void drawCircle(Pixel2D center, double rad, int color) {
+
+	}
+
+	@Override
+	public void drawLine(Pixel2D p1, Pixel2D p2, int color) {
+
+	}
+
+	@Override
+	public void drawRect(Pixel2D p1, Pixel2D p2, int color) {
+
+	}
+
+	@Override
+	public boolean equals(Object ob) {
+		if (this == ob) return true;
+		if (!(ob instanceof Map2D)) return false;
+
+		Map2D other = (Map2D)ob;
+		if (!this.sameDimensions(other)) return false;
+
+		for (int x = 0; x < getWidth(); x++) {
+			for (int y = 0; y < getHeight(); y++) {
+				if (this.getPixel(x, y ) != other.getPixel(x, y)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	@Override
+	/**
 	 * Fills this map with the new color (new_v) starting from p.
 	 * https://en.wikipedia.org/wiki/Flood_fill
 	 */
@@ -148,12 +178,12 @@ public class Map implements Map2D, Serializable{
 
 		return ans;
 	}
-    @Override
-    public Map2D allDistance(Pixel2D start, int obsColor, boolean cyclic) {
-        Map2D ans = null;  // the result.
+	@Override
+	public Map2D allDistance(Pixel2D start, int obsColor, boolean cyclic) {
+		Map2D ans = null;  // the result.
 
-        return ans;
-    }
+		return ans;
+	}
 	////////////////////// Private Methods ///////////////////////
 
 }
